9cd73c80f04b617a94c486cd68189a993c68dc30
jeymesonpessoa <104586748+jeymesonpessoa@users.noreply.github.com>
2022-06-14 14:21:14 -0300

    refatoração do arquivo de massa de testes

--- resources/factories/Users.py
+++ resources/factories/Users.py
@@ -7,44 +7,41 @@ def get_hashed_pass(password):
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(8))
     return hashed
 
-def factory_user():
-    return {
-        'name': fake.first_name(),
-        'lastname': fake.last_name(),
-        'email': fake.free_email(),
-        'password': 'pwd123'
-    }
+def factory_user(target):
 
-def factory_wrong_email():
+    data = {
 
-    first_name = fake.first_name()
-
-    return {
-        'name': first_name,
-        'lastname': fake.last_name(),
-        'email': first_name.lower() + '$gmail.com',
-        'password': 'pwd123'
-    }
-
-def factory_user_login():
-    return {
-        'name': 'Jeymeson',
-        'lastname': 'Costa',
-        'email': 'jeymeson@hotmail.com',
-        'password': 'pwd123'
-    }
-
-def factory_user_begeek():
-    return {
-        'name': 'Kim',
-        'lastname': 'Dotcom',
-        'email': 'kim@dot.com',
-        'password': 'pwd123',
-        'geek_profile': {
-            'whats': '11999999999',
-            'desc': 'Seu computador está lento? Talvez seja um vírus, posso fazer a manutenção do seu pc! Entre em contato comigo! :)',
-            'printer_repair': 'Sim',
-            'work': 'Remoto',
-            'cost': '100'
+        'faker': {
+            'name': fake.first_name(),
+            'lastname': fake.last_name(),
+            'email': fake.free_email(),
+            'password': 'pwd123'
+        },
+        'wrong_email': {
+            'name': 'Pedro',
+            'lastname': 'de Lara',
+            'email': 'pedro_dl*hotmail.com',
+            'password': 'abc123'
+        },
+        'login': {
+            'name': 'Jeymeson',
+            'lastname': 'Costa',
+            'email': 'jeymeson@hotmail.com',
+            'password': 'pwd123'
+        },
+        'be_geek': {
+            'name': 'Kim',
+            'lastname': 'Dotcom',
+            'email': 'kim@dot.com',
+            'password': 'pwd123',
+            'geek_profile': {
+                'whats': '11999999999',
+                'desc': 'Seu computador está lento? Talvez seja um vírus, posso fazer a manutenção do seu pc! Entre em contato comigo! :)',
+                'printer_repair': 'Sim',
+                'work': 'Remoto',
+                'cost': '100'
+            }
         }
     }
+
+    return data[target]
